d337a1dc39227deb514c7d450474ecb9cf6d7bda
Mametsa-92 <82805724+Mametsa-92@users.noreply.github.com>
2021-10-11 15:09:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
 const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
 const header =document.querySelector('.header.container');
 
-hamburger.addEventListener('touchstart click', ()=>{
+hamburger.addEventListener('click', ()=>{
  hamburger.classList.toggle('active');
  mobile_menu.classList.toggle('active');
 });
@@ -20,7 +20,7 @@ document.addEventListener('scroll', () => {
 });
 
 menu_item.forEach((item) =>{
-    item.addEventListener('touchstart click', () => {
+    item.addEventListener('click', () => {
         hamburger.classList.toggle('active');
         mobile_menu.classList.toggle('active');
     })
